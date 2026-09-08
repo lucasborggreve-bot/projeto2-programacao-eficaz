aec302d77eabdcc8a16ce17b0f03c4933208ab20
Rafael Duarte <rafael.duarte0905@gmail.com>
2026-09-08 10:43:31 -0300

    teste atualizar imovel not found feito

--- test_api.py
+++ test_api.py
@@ -159,3 +159,27 @@ def test_atualizar_imovel_ok(mock_conectar_banco, client):
     mock_conn.commit.assert_called_once()
     mock_cursor.close.assert_called_once()
     mock_conn.close.assert_called_once()
+
+@patch('api.get_connection')
+def test_atualizar_imovel_not_found(mock_conectar_banco, client):
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+
+    mock_cursor.rowcount = 0
+    mock_conectar_banco.return_value = mock_conn
+
+    payload = {'logradouro': 'Rafael Duarte', 'tipo_logradouro': 'sei la', 'bairro': 'ulala', 'cidade': 'sp né', 'cep': '01201', 'tipo': 'mansao', 'valor': '000999', 'data_aquisicao': '2026-05-09'}
+    response = client.put('/imoveis/999', json=payload)
+
+    assert response.status_code == 404
+    assert response.get_json() == {'erro': 'Imóvel não encontrado'}
+
+    mock_cursor.execute.assert_called_once_with(
+        "UPDATE imoveis SET logradouro = ?, tipo_logradouro = ?, bairro = ?, cidade = ?, cep = ?, tipo = ?, valor = ?, data_aquisicao = ? WHERE id = ?",
+        ('Rafael Duarte', 'sei la', 'ulala', 'sp né', '01201', 'mansao', '000999', '2026-05-09', 1)
+    )
+
+    mock_conn.commit.assert_called_once()
+    mock_cursor.close.assert_called_once()
+    mock_conn.close.assert_called_once()
